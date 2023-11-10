56435fbace4ed1978091fa142afdd022be38f43e
Chave <rchave2801@hotmail.com>
2023-11-10 10:41:10 -0500

    Initial commit

---
+++ src/main/java/co/com/idi/springbootbasics/SpringbootbasicsApplication.java
@@ -0,0 +1,13 @@
+package co.com.idi.springbootbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootbasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootbasicsApplication.class, args);
+	}
+
+}
